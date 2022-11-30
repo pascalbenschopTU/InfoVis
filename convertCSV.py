5f1b7959086b8a65817b4534df4292619f1d857f
pascal <pascal3benschop@gmail.com>
2022-11-30 11:51:12 +0100

    Adding python support for preprocessing

---
+++ convertCSV.py
@@ -0,0 +1,30 @@
+import sys
+import pandas as pd
+
+def makeCSV(filename):
+
+    def getNumber(string):
+        return int(string.split(",")[0])
+    
+    def getYear(string):
+        return int(string.split("-")[2])
+
+    df  = pd.read_csv(filename, delimiter=";")
+    df = df[df['HOEDANIGHEID_OMSCHRIJVING'] == 't.o.v. Normaal Amsterdams Peil']
+
+    keep_col = ['X','Y','NUMERIEKEWAARDE', 'WAARNEMINGDATUM']
+    new_f = df[keep_col]
+    new_f = new_f[new_f['NUMERIEKEWAARDE'] < 10000] # Remove erroneous 99999999 values
+    new_f['WAARNEMINGDATUM'] = new_f['WAARNEMINGDATUM'].apply(getYear) # Add datum in year value
+    
+    small_f = new_f.groupby(['X', 'Y', 'WAARNEMINGDATUM'], as_index=False).mean() # Take mean water level of location
+    small_f['X'] = small_f['X'].apply(getNumber)
+    small_f['Y'] = small_f['Y'].apply(getNumber)
+
+    small_f.to_csv("waterlevel" + filename)
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    if isinstance(arg, str):
+        makeCSV(arg)
