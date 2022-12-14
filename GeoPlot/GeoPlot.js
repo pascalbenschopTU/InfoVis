73d7cf0a5bf4725b6157133262bd0ac67d3cb33f
Marit Radder <m.e.radder@student.tudelft.nl>
2022-12-14 10:26:54 +0100

    get height data

--- GeoPlot/GeoPlot.js
+++ GeoPlot/GeoPlot.js
@@ -20,10 +20,10 @@ const button = document.getElementById('toggleDensity')
 const iframe = document.getElementById('iframe')
 
 
-var thsd = d3.format("d"); 
+var thsd = d3.format("d");
 
 // Scaling is from -40cm NAP to 200cm NAP
-var waterColor = d3.scaleLinear().domain([-50 ,0, 200, 4000]).range(["blue", "green", "yellow", "orange"])
+var waterColor = d3.scaleLinear().domain([-50, 0, 200, 4000]).range(["blue", "green", "yellow", "orange"])
 var densityColor = d3.scaleLinear().domain([50, 100, 500]).range(["lightblue", "green", "brown"])
 
 var width = 900,
@@ -45,13 +45,13 @@ Promise.all([
     d3.json("https://cartomap.github.io/nl/wgs84/gemeente_2022.topojson"),
     d3.json("data/population_density.json"),
     d3.csv('data/waterlevels.csv')
-]).then(function(data) {
-    
+]).then(function (data) {
+
     function showNetherlands(data) {
         // For 2009 version use:
         // var gemeenten = topojson.feature(nlgemeenten2009, nlgemeenten2009.objects.gemeenten);
         var gemeenten = topojson.feature(data[0], data[0].objects.gemeente_2022);
-    
+
         svg.append("g")
             .attr("class", "land")
             .selectAll("path")
@@ -61,32 +61,32 @@ Promise.all([
             .style("fill", "#85BB65")
             .style("stroke", "#9B65BA")
     }
-    
+
     function showDensity(data) {
         var density = topojson.feature(data[1], data[1].objects.population_density);
 
         svg.append("g")
-                .attr("class", "density")
-                .attr("id", "density")
-                .selectAll("path")
-                .data(density.features)
-                .enter().append("path")
-                .attr("d", path)
-                .style("fill", function(data) {
-                    return densityColor(data.properties.aantal_inwoners)
-                })
+            .attr("class", "density")
+            .attr("id", "density")
+            .selectAll("path")
+            .data(density.features)
+            .enter().append("path")
+            .attr("d", path)
+            .style("fill", function (data) {
+                return densityColor(data.properties.aantal_inwoners)
+            })
     }
 
     function showWaterLevels(data) {
         svg.selectAll("circle")
             .data(data[2]).enter()
             .append("circle")
-            .attr("cx", function(d) {
+            .attr("cx", function (d) {
                 var c = Utm2Wgs(d.X, d.Y, 31)
                 var p = projection(c)
                 return p[0]
             })
-            .attr("cy", function(d) {
+            .attr("cy", function (d) {
                 var c = Utm2Wgs(d.X, d.Y, 31)
                 var p = projection(c)
                 return p[1]
@@ -99,7 +99,7 @@ Promise.all([
             .style("stroke", "black")
             .style("position", "absolute")
             // Show the water level when hovering over a point
-            .on("mouseover", function(d) {
+            .on("mouseover", function (d) {
                 var xPosition = d.x - 900;
                 var yPosition = d.y > 250 ? d.y - 200 : d.y - 100;
                 svg.append("text")
@@ -113,19 +113,19 @@ Promise.all([
                 d3.select(this)
                     .attr("class", "selected");
             })
-            .on("mouseout", function(d) {
+            .on("mouseout", function (d) {
                 d3.select("#tooltip").remove();
                 d3.select(this)
-                .transition()
-                .attr("class", "waterlevel")
-                .duration(250)
+                    .transition()
+                    .attr("class", "waterlevel")
+                    .duration(250)
             });
 
         slider.addEventListener('input', event => {
             // Transform water level points
             svg.selectAll(".waterlevel")
-                .on("mouseover", function(d) {
-                
+                .on("mouseover", function (d) {
+
                     var xPosition = d.x - 900;
                     var yPosition = d.y > 250 ? d.y - 200 : d.y - 100;
                     svg.append("text")
@@ -141,41 +141,41 @@ Promise.all([
                 })
             svg.selectAll(".waterlevel").transition()
                 .duration(750)
-                
-                .attr("cx", function(data) {
+
+                .attr("cx", function (data) {
                     var c = Utm2Wgs(data.X, data.Y, 31)
                     var p = projection(c)
                     return p[0]
                 })
-                .attr("cy", function(data) {
+                .attr("cy", function (data) {
                     var c = Utm2Wgs(data.X, data.Y, 31)
                     var p = projection(c)
                     return p[1]
                 })
-                .style("fill", function(data) {
+                .style("fill", function (data) {
                     return waterColor(data[event.target.value])
                 })
-                .style("display", function(data) {
+                .style("display", function (data) {
                     if (data[event.target.value] == "") {
                         return "None";
                     }
                     return "Block";
                 })
-                    
+
 
             const display = document.getElementById('display')
             display.innerText = event.target.value;
         })
     }
-    
+
     button.addEventListener("click", _ => {
-        if (button.value=="on") {
-            button.value="off";
+        if (button.value == "on") {
+            button.value = "off";
             document.getElementById("density").remove();
-            
+
         }
-        else if (button.value=="off") {
-            button.value="on";
+        else if (button.value == "off") {
+            button.value = "on";
             svg.selectAll("circle").remove()
             showDensity(data)
             showWaterLevels(data)
@@ -196,12 +196,12 @@ Promise.all([
 
         svg.append("circle")
             .attr("class", "highlighted")
-            .attr("cx", function() {
+            .attr("cx", function () {
                 var c = Utm2Wgs(X, Y, 31)
                 var p = projection(c)
                 return p[0]
             })
-            .attr("cy", function(data) {
+            .attr("cy", function (data) {
                 var c = Utm2Wgs(X, Y, 31)
                 var p = projection(c)
                 return p[1]
@@ -211,14 +211,17 @@ Promise.all([
             .attr("stroke", "purple")
             .attr("stroke-width", "5px")
 
-        
-        focusHeightMap(X, Y)
-        plotWaterLevelGraph(X)
+        plotWaterLevelGraph(X, Y)
     }
 
-    function plotWaterLevelGraph(X) {
+    function plotWaterLevelGraph(X, Y) {
         var waterlevels = data[2].find(item => item.X == X)
         var waterlevel_data = []
+        var wgs = Utm2Wgs(X, Y, 31)
+        var rd = rijksdriehoek(wgs[0], wgs[1])
+        var height = httpGet('https://ahn.arcgisonline.nl/arcgis/rest/services/AHNviewer/AHN3_r/ImageServer/identify?f=json&geometry=%7B%22x%22%3A' + rd[0] +'%2C%22y%22%3A' + rd[1] +'%2C%22spatialReference%22%3A%7B%22wkid%22%3A28992%2C%22latestWkid%22%3A28992%7D%7D&returnGeometry=true&returnCatalogItems=true&geometryType=esriGeometryPoint&pixelSize=%7B%22x%22%3A50.80010160020321%2C%22y%22%3A50.8001016002033%2C%22spatialReference%22%3A%7B%22wkid%22%3A28992%2C%22latestWkid%22%3A28992%7D%7D&renderingRules=%5B%7B%22rasterFunction%22%3A%22AHN2%20-%20Color%20Ramp%20C%22%7D%5D').value
+        height *= 100
+        console.log(height)
         for (var i = 2010; i <= 2022; i++) {
             if (waterlevels[i] != "") {
                 waterlevel_data.push([i, waterlevels[i]])
@@ -226,15 +229,14 @@ Promise.all([
         }
 
         var w = new WeatherGraph(waterlevel_data, "waterlevels", "blue", "red")
-        
+
         w.plotDataGraph()
     }
 
-    function focusHeightMap(X,Y) {
-        var wgs = Utm2Wgs(X, Y, 31)
-        var rd = rijksdriehoek(wgs[0], wgs[1])
-        
-        var src = "https://ahn.arcgisonline.nl/ahnviewer/?center=" + rd[0] + "%2C"+ rd[1] + "%2C28992&level=6&locale=en"
-        iframe.src = src
+    function httpGet(theUrl) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", theUrl, false); // false for synchronous request
+        xmlHttp.send(null);
+        return JSON.parse(xmlHttp.responseText);
     }
 })
